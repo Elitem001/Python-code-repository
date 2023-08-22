332228ade15979152630be2a0bfee9cbc9e676f8
Elitem <111205088+Elitem001@users.noreply.github.com>
2023-08-22 16:41:00 +0200

    Add files via upload

---
+++ Projekt1.py
@@ -0,0 +1,19 @@
+x = 1 #int
+y = 3.0 #float
+z = "7" #str
+
+print(z*3)
+print("X is "+str(x))
+print("Y is " + str(y))
+
+name = input("What is your name ?")
+age = int(input("How old are u ?: "))
+print("Hi " + name)
+age = age+ 5
+print("You are "+str(age)+" years old")
+
+height =float(input("How tall are u ?: "))
+print("Your height is : "+str(height))
+
+
+
