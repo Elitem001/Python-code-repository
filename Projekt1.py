d5e109541e8136112d70ad7e8b66422062da2ea1
MichalB <111205088+Elitem001@users.noreply.github.com>
2025-12-20 13:32:07 +0100

    Projekt1 po dlhom case upraveny :)))))
    Added comments to explain variable types and clarified print statements.

--- Projekt1.py
+++ Projekt1.py
@@ -1,19 +1,20 @@
 x = 1 #int
 y = 3.0 #float
 z = "7" #str
-
+# learning about integers, floats and strings
 print(z*3)
-print("X is "+str(x))
+print("X is "+ str(x))
 print("Y is " + str(y))
 
 name = input("What is your name ?")
 age = int(input("How old are u ?: "))
-print("Hi " + name)
+print("Hi" + name)
 age = age+ 5
-print("You are "+str(age)+" years old")
+print("You are "+ str(age) +" years old")
 
-height =float(input("How tall are u ?: "))
-print("Your height is : "+str(height))
+height = float(input("How tall are u ?: "))
+print("Your height is : "+ str(height))
+
 
 
 
